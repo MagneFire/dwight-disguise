d4bc5a6fbf138470ce13d3e1e9d812d3d0ddde01
MagneFire <IDaNLContact@gmail.com>
2020-09-24 13:29:05 +0200

    Rename 0099-dwight-disguise.* -> 099-dwight-disguise.*

--- push.sh
+++ push.sh
@@ -2,10 +2,10 @@
 if [ "$1" == "adb" ]
 then
     echo "Pushing files using ADB mode..."
-    adb push 0099* /usr/share/asteroid-launcher/wallpapers/
+    adb push 099* /usr/share/asteroid-launcher/wallpapers/
     adb push dwight-disguise/ /usr/share/asteroid-launcher/wallpapers/
 else
     echo "Pushing files using Developer mode..."
-    scp 0099* root@192.168.2.15:/usr/share/asteroid-launcher/wallpapers/
+    scp 099* root@192.168.2.15:/usr/share/asteroid-launcher/wallpapers/
     scp -r dwight-disguise root@192.168.2.15:/usr/share/asteroid-launcher/wallpapers/
 fi
